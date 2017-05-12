28f25cab79c59ac2c009d5be222a7b714292eb16
Robert Brucker <brucker.rob@gmail.com>
2017-05-12 19:50:33 -0400

    get last record via order, correct end_time value on last record response

--- app/services/category_records/get_last_records_for_category_service.rb
+++ app/services/category_records/get_last_records_for_category_service.rb
@@ -6,12 +6,12 @@ module CategoryRecords
         categories = Category.where(:user_id => params[:user_id])
 
         categories.each do |category|
-          record = CategoryRecord.where(:category_id => category.id, :user_id => params[:user_id]).last
+          record = CategoryRecord.where(:category_id => category.id, :user_id => params[:user_id]).order('id desc').first
           last_record = {}
           last_record["category_name"] = category.name
           if record
             last_record["start_time"] = record.start_time
-            last_record["end_time"] = record.start_time
+            last_record["end_time"] = record.end_time
             last_record["total_time"] = record.total_time
           end
           last_record_array << last_record
